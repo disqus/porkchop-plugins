e879ce26548e3bf9d5ed250f2689f1270cf42b9e
Scott Smith <scott@disqus.com>
2011-10-04 20:10:41 -0700

    for #2 and some misc cleanup

--- networkinterface.py
+++ networkinterface.py
@@ -12,50 +12,26 @@ def sub(a, b, inter):
 
 def read_info():
   data = deepDict()
-  recv_stat_fields = [
-    'bytes',
-    'packets',
-    'errs',
-    'drop',
-    'fifo',
-    'frame',
-    'compressed',
-    'multicast'
-  ]
-  xmit_stat_fields = [
-    'bytes',
-    'packets',
-    'errs',
-    'drop',
-    'fifo',
-    'colls',
-    'carrier',
-    'compressed'
-  ]
 
   f = open('/proc/net/dev', 'r')
   output = f.readlines()
-  output.pop(0); output.pop(0)
+  output.pop(0)
 
-  for line in output:
+  keys = output[0].replace('|','').split()[1:]
+  recv_keys = tuple(keys[0:8])
+  xmit_keys = tuple(keys[8:])
+
+  for line in output[1:]:
     fields = line.split()
-    interface = fields.pop(0).strip()
-    interface = interface[0:len(interface)-1]
-
-    for pos in xrange(len(recv_stat_fields)):
-      stat = recv_stat_fields[pos]
-      data[interface]['recv'][stat] = fields[pos]
-
-    for pos in xrange(len(xmit_stat_fields)):
-      stat = xmit_stat_fields[pos]
-      data[interface]['xmit'][stat] = fields[pos+len(recv_stat_fields)]
+    iface = fields.pop(0).strip(':')
+    recv_tup = tuple(fields[0:8])
+    xmit_tup = tuple(fields[8:])
+    data[iface]['recv'] = dict(zip(recv_keys, recv_tup))
+    data[iface]['xmit'] = dict(zip(xmit_keys, xmit_tup))
 
   return data
 
 class NetworkinterfacePlugin(PorkchopPlugin):
-  def __init__(self):
-    self.refresh = 1
-
   def get_data(self):
     data = deepDict()
 
@@ -69,12 +45,12 @@ class NetworkinterfacePlugin(PorkchopPlugin):
 
     self.__class__._cache = cur = read_info()
 
-
     for iface in cur.keys():
       for typ in cur[iface].keys():
         for stat in cur[iface][typ].keys():
-          data[iface][typ]['%s_per_second' % stat] = sub(prev[iface][typ][stat],
-                                                         cur[iface][typ][stat],
-                                                         delta)
+          label = '%s_per_second' % stat
+          data[iface][typ][label] = sub(prev[iface][typ][stat],
+                                        cur[iface][typ][stat],
+                                        delta)
 
     return data
