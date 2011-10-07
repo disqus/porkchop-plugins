b4cc5006418617123a56cd6b1e298dbca487ad1b
Scott Smith <scott@disqus.com>
2011-10-06 22:15:46 -0700

    count threads and some other random stuff

--- processes.py
+++ processes.py
@@ -6,7 +6,7 @@ from porkchop.plugin import PorkchopPlugin
 def sub(a, b, inter):
   return (b - a) / inter if (b - a) > 0 else 0
 
-def read_info():
+def processes_count():
   count = 0
 
   f = open('/proc/stat', 'r')
@@ -18,21 +18,39 @@ def read_info():
   f.close()
   return count
 
+def thread_count(f):
+  threads = 0
+
+  try:
+    f = open(f, 'r')
+  except IOError:
+    pass
+
+  for line in f:
+    if line.startswith('Threads:'):
+      count = int(line.split()[1])
+
+  f.close()
+  return count
+
 class ProcessesPlugin(PorkchopPlugin):
   def get_data(self):
     data = {}
 
-    data['count'] = len(glob.glob('/proc/[1-9]*'))
+    data['count'] = data['threads'] = 0
+    for piddir in glob.glob('/proc/[1-9]*'):
+      data['count'] += 1
+      data['threads'] += thread_count('%s/status' % piddir)
 
     if not self.__class__._cache:
-      prev = read_info()
+      prev = processes_count()
       delta = 1
       time.sleep(delta)
     else:
       prev = self.__class__._cache
       delta = int(time.time() - self.__class__._lastrefresh)
 
-    self.__class__._cache = cur = read_info()
+    self.__class__._cache = cur = processes_count()
 
     data['forkrate'] = sub(prev, cur, delta)
 
