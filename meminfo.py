4ac34603ef55da0a05b98bc70cc40f7ec4cf8459
Scott Smith <scott@disqus.com>
2011-10-12 00:21:45 -0700

    calculate used memory and swap

--- meminfo.py
+++ meminfo.py
@@ -9,4 +9,10 @@ class MeminfoPlugin(PorkchopPlugin):
         lhs = lhs.replace('(', '_').replace(')', '').lower()
         data[lhs] = rhs.strip().split()[0]
 
+      data['used'] = int(data['memtotal']) - \
+                     (int(data['memfree']) + \
+                      int(data['buffers']) + \
+                      int(data['cached']))
+      data['swapused'] = int(data['swaptotal']) - int(data['swapfree'])
+
     return data
