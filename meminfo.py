866c2c17cc63c87f9f99b10818ce0157326dc662
Scott Smith <scott@disqus.com>
2011-10-12 19:47:22 -0700

    rename metric for used memory

--- meminfo.py
+++ meminfo.py
@@ -9,7 +9,7 @@ class MeminfoPlugin(PorkchopPlugin):
         lhs = lhs.replace('(', '_').replace(')', '').lower()
         data[lhs] = rhs.strip().split()[0]
 
-      data['used'] = int(data['memtotal']) - \
+      data['memused'] = int(data['memtotal']) - \
                      (int(data['memfree']) + \
                       int(data['buffers']) + \
                       int(data['cached']))
