b91ebdc58330b682c33bd66014251f3ad8a9ce30
John Watson <john@disqus.com>
2011-10-28 17:49:06 -0700

    Add ElasticSearch plugin

---
+++ elasticsearch.py
@@ -0,0 +1,17 @@
+import json
+import requests
+from porkchop.plugin import PorkchopPlugin
+
+class ElasticSearchPlugin(PorkchopPlugin):
+  def get_data(self):
+    stats_url = self.config.get('elasticsearch', {}).get('stats_url',
+      'http://localhost:9200/_cluster/nodes/_local/stats')
+
+    raw_stats = json.loads(requests.get(stats_url).content)
+
+    # raw_stats['nodes'][<HASH>]
+    # The <HASH> is generated at server start and since it is the only key
+    #   just list keys and grab the first one
+    stats = { raw_stats['cluster_name']: raw_stats['nodes'][raw_stats['nodes'].keys()[0]] }
+    
+    return stats
