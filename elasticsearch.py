5bc0927204c47c0baf760278dd22d2a168dde740
John Watson <john@disqus.com>
2011-10-28 18:29:45 -0700

    Un-camel-case plugin and remove extraneous var

--- elasticsearch.py
+++ elasticsearch.py
@@ -2,7 +2,7 @@ import json
 import requests
 from porkchop.plugin import PorkchopPlugin
 
-class ElasticSearchPlugin(PorkchopPlugin):
+class ElasticsearchPlugin(PorkchopPlugin):
   def get_data(self):
     stats_url = self.config.get('elasticsearch', {}).get('stats_url',
       'http://localhost:9200/_cluster/nodes/_local/stats')
@@ -12,6 +12,4 @@ class ElasticSearchPlugin(PorkchopPlugin):
     # raw_stats['nodes'][<HASH>]
     # The <HASH> is generated at server start and since it is the only key
     #   just list keys and grab the first one
-    stats = { raw_stats['cluster_name']: raw_stats['nodes'][raw_stats['nodes'].keys()[0]] }
-    
-    return stats
+    return { raw_stats['cluster_name']: raw_stats['nodes'][raw_stats['nodes'].keys()[0]] }
