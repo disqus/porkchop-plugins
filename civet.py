5df8baf4fa5756636a66c9e625e539f442235caa
John Watson <john@disqus.com>
2012-08-15 11:25:47 -0700

    Civet plugin

---
+++ civet.py
@@ -0,0 +1,29 @@
+import json
+import socket
+
+from porkchop.plugin import PorkchopPlugin
+
+
+class CivetPlugin(PorkchopPlugin):
+    def get_data(self):
+        instance_config = self.config.get('civet', {}).get('instance', 'localhost:7201')
+        host, port = instance_config.split(':')
+
+        json_data = ''
+        try:
+            s = socket.create_connection((host, int(port)), timeout=1)
+            s.sendall('sample\n')
+
+            while 1:
+                raw_data = s.recv(1024)
+                if not raw_data:
+                    break
+                json_data += raw_data
+        except socket.error:
+            pass
+        finally:
+            s.close()
+
+        data = json.loads(json_data)
+
+        return data
