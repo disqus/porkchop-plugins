14cddf65c39ecd936bf1bf890a884f6637a48cc1
John Watson <john@disqus.com>
2012-08-21 18:25:46 -0700

    Monitor CPU usage by a filter
    Filters:
    - exe: The executable of the process `/proc/<pid>/exe` using fnmatch
    - cmdline: The command used to run the process `/proc/<pid>/cmdline`
      using a regex
    - pidfile: A pidfile

---
+++ cpubyprocess.py
@@ -0,0 +1,172 @@
+import os
+import re
+
+from collections import defaultdict
+from fnmatch import fnmatch
+from time import sleep
+
+from porkchop.plugin import PorkchopPlugin
+
+
+class Filter(object):
+    def __init__(self, filter):
+        self.filter = filter
+
+    def match(self, on):
+        return self._filter(on)
+
+    def _filter(self, on):
+        raise NotImplementedError()
+
+    @staticmethod
+    def is_compatible(filter_string):
+        raise NotImplementedError()
+
+
+class PIDFileFilter(Filter):
+    def __init__(self, *args, **kwargs):
+        self.last_mtime = 0
+        return super(PIDFileFilter, self).__init__(*args, **kwargs)
+
+    @staticmethod
+    def is_compatible(filter_string):
+        return isinstance(filter_string, basestring) and os.access(filter_string, os.R_OK)
+
+    def _filter(self, on):
+        return str(self.pid) == on.split(os.sep)[2]
+
+    @property
+    def pid(self):
+        try:
+            st_mtime = os.stat(self.filter).st_mtime
+        except OSError:
+            return None
+
+        if st_mtime > self.last_mtime:
+            try:
+                with open(self.filter) as fp:
+                    self._pid = int(fp.read().strip())
+                    self.last_mtime = st_mtime
+            except IOError:
+                return None
+
+        return self._pid
+
+
+class EXEFilter(Filter):
+    @staticmethod
+    def is_compatible(filter_string):
+        return isinstance(filter_string, basestring)
+
+    def _filter(self, on):
+        try:
+            return fnmatch(os.path.realpath(os.path.join(on, 'exe')), self.filter)
+        except OSError:
+            return False
+
+
+class CMDLineFilter(Filter):
+    def __init__(self, filter):
+        super(CMDLineFilter, self).__init__(filter)
+
+        self.filter_re = re.compile(self.filter)
+
+    @staticmethod
+    def is_compatible(filter_string):
+        try:
+            re.compile(filter_string)
+        except re.error:
+            return False
+        else:
+            return True
+
+    def _filter(self, on):
+        with open(os.path.join(on, 'cmdline')) as fp:
+            cmdline = fp.read().strip()
+
+        cmdline = ' '.join(cmdline.split('\x00'))
+        return self.filter_re.search(cmdline) is not None
+
+
+FILTERS = {
+    'pidfile': PIDFileFilter,
+    'exe': EXEFilter,
+    'cmdline': CMDLineFilter,
+}
+
+_CLOCK_RATE = os.sysconf(os.sysconf_names['SC_CLK_TCK'])
+_NUM_CPUS = os.sysconf('SC_NPROCESSORS_ONLN')
+
+
+def get_proc_jiffies(pid):
+    with open('/proc/%s/stat' % pid) as proc_stat:
+        stat = proc_stat.read().strip()
+
+    stat = stat[stat.find(')') + 2:]
+    vals = stat.split(' ')
+
+    ujiffies = float(vals[11]) / _CLOCK_RATE
+    sjiffies = float(vals[12]) / _CLOCK_RATE
+
+    return ujiffies + sjiffies
+
+
+def get_system_jiffies():
+    with open('/proc/stat') as proc_stat:
+        stat = proc_stat.read().split()
+
+    stat = stat[1:8]
+    return sum([float(s) / _CLOCK_RATE for s in stat])
+
+
+class CPUByProcessPlugin(PorkchopPlugin):
+    __cache = defaultdict(dict)
+
+    def get_data(self):
+        filters = {}
+        for process, mask in self.config.get('processes', {}).iteritems():
+            if ':' not in mask:
+                continue
+
+            filter_type, filter_string = mask.split(':', 1)
+
+            if filter_type not in FILTERS:
+                continue
+
+            if FILTERS[filter_type].is_compatible(filter_string):
+                filters[process] = FILTERS[filter_type](filter_string)
+
+        if not filters:
+            return {}
+
+        data = defaultdict(float)
+
+        for proc in os.listdir('/proc'):
+            proc_path = os.path.join('/proc', proc)
+            if not os.path.isdir(proc_path) or not proc.isdigit():
+                continue
+
+            for name, proc_filter in filters.iteritems():
+                if proc_filter.match(proc_path):
+                    if proc not in self.__cache:
+                        self.__cache[proc]['proc'] = get_proc_jiffies(proc)
+                        self.__cache[proc]['sys'] = get_system_jiffies()
+                        sleep(0.1)
+
+                    proc_jiffies = get_proc_jiffies(proc)
+                    sys_jiffies = get_system_jiffies()
+
+                    delta_proc = proc_jiffies - self.__cache[proc]['proc']
+                    delta_sys = sys_jiffies - self.__cache[proc]['sys']
+
+                    if delta_proc < 0.0:
+                        sleep(0.1)
+                        delta_proc = get_proc_jiffies(proc) - proc_jiffies
+                        delta_sys = get_system_jiffies() - sys_jiffies
+
+                    try:
+                        data[name] += (delta_proc / delta_sys) * 100.0 * _NUM_CPUS
+                    except ZeroDivisionError:
+                        data[name] += 0.0
+
+        return data
