f0f19ce9b24854b9469a926e4fc94ea9bf226c23
John Watson <john@disqus.com>
2012-10-04 00:36:22 -0700

    make status url configurable

--- nginx_http_push_stream.py
+++ nginx_http_push_stream.py
@@ -8,7 +8,8 @@ class NginxHttpPushStreamPlugin(PorkchopPlugin):
     METRIC_KEYS = frozenset(['channels', 'broadcast_channels', 'published_messages', 'subscribers', 'uptime'])
 
     def get_data(self):
-        f = urllib2.urlopen('http://127.0.0.1/status', timeout=1)
+        status_url = self.config.get('status_url', 'http://127.0.0.1/push-stream-status')
+        f = urllib2.urlopen(status_url, timeout=1)
 
         try:
             raw_data = json.load(f)
